1f639cc7786acf92c7f18246d4ba75ec6191719c
superyang713 <yangdai713@aim.com>
2018-08-19 01:23:04 -0400

    set up install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,50 @@
-# Still working on it.
+#!/bin/sh
+
+download_repo()
+{
+    echo "Downloading all config files from
+    https://github.com/superyang713/vimrc-and-plugins"
+}
+
+install_vimrc()
+{
+    echo "installing vimrc........"
+}
+
+install_vim_plugins()
+{
+    echo "installing all vim plugins"
+}
+
+install_tmux_config()
+{
+    echo "installing tmux.config........."
+}
+
+install_zshrc()
+{
+    echo "installing zsh......."
+    echo "installing oh-my-zsh......"
+    echo "install zshrc and plugins......"
+}
+
+help_message()
+{
+    echo "If text is not displayed properly, please install the font"
+}
+
 echo "The installation will replace your current vimrc, zshrc, and vim plugins,
 are you sure to proceed? (y/n)"
 
 read resp
 
 if [ $resp = "y" ]; then
-    git clone https://github.com/superyang713/vimrc-and-plugins
+    download_repo
+    install_vimrc
+    install_vim_plugins
+    install_tmux_config
+    install_zshrc
+    help_message
 else
     echo "oh no"
 fi;
