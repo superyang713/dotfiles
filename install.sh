733f598c2f57207c554940b3ea11aa7e1e644a98
superyang713 <yangdai713@aim.com>
2018-08-19 16:28:43 -0400

    set up install.sh

--- install.sh
+++ install.sh
@@ -2,49 +2,55 @@
 
 download_repo()
 {
+    cd ~
     echo "Downloading all config files from Yang's repo"
-    git clone https://github.com/superyang713/vimrc-and-plugins
+    git clone --quiet https://github.com/superyang713/vimrc-and-plugins
 
     echo "Preparing for vim plugins download"
     cd vimrc-and-plugins/vim_plugins/bundle/
-    echo "Downloading nerdtree..."
-    git clone --quiet https://github.com/jistr/vim-nerdtree-tabs.git
-    git clone --quiet https://github.com/scrooloose/nerdtree.git
-    echo "Downloading powerline..."
-    git clone --quiet https://github.com/powerline/powerline.git
-    echo "Downloading sensible..."
-    git clone --quiet https://github.com/tpope/vim-sensible.git
-    echo "Downloading python-mode..."
-    git clone --quiet --recursive https://github.com/python-mode/python-mode
-    echo "Downloading sparkup..."
-    git clone --quiet https://github.com/rstacruz/sparkup.git
-    echo "Downloading tlib_vim..."
-    git clone --quiet https://github.com/tomtom/tlib_vim.git
-    echo "Downloading snippets..."
-    git clone --quiet https://github.com/honza/vim-snippets.git
-    echo "Downloading snipmate..."
-    git clone --quiet https://github.com/garbas/vim-snipmate.git
-    echo "Downloading ctrlp..."
-    git clone --quiet https://github.com/kien/ctrlp.vim.git
-    echo "Downloading solarize..."
-    git clone --quiet https://github.com/altercation/vim-colors-solarized.git
 
-    cd ../../../
+    echo "Downloading nerdtree..."
+    git submodule add --quiet https://github.com/jistr/vim-nerdtree-tabs.git
+    git submodule add --quiet https://github.com/scrooloose/nerdtree.git
+    echo "Downloading powerline..."
+    git submodule add --quiet https://github.com/powerline/powerline.git
+    echo "Downloading sensible..."
+    git submodule add --quiet https://github.com/tpope/vim-sensible.git
+    echo "Downloading python-mode..."
+    git clone --recursive --quiet https://github.com/python-mode/python-mode
+    echo "Downloading sparkup..."
+    git submodule add --quiet https://github.com/rstacruz/sparkup.git
+    echo "Downloading tlib_vim..."
+    git submodule add --quiet https://github.com/tomtom/tlib_vim.git
+    echo "Downloading snippets..."
+    git submodule add --quiet https://github.com/honza/vim-snippets.git
+    echo "Downloading snipmate..."
+    git submodule add --quiet https://github.com/garbas/vim-snipmate.git
+    echo "Downloading ctrlp..."
+    git submodule add --quiet https://github.com/kien/ctrlp.vim.git
+    echo "Downloading solarize..."
+    git submodule add --quiet https://github.com/altercation/vim-colors-solarized.git
+
+    cd ~/vimrc-and-plugins
 }
 
 install_vimrc()
 {
     echo "installing vimrc"
+    cp vimrc ~/.vimrc
 }
 
 install_vim_plugins()
 {
     echo "installing all vim plugins"
+    rm -rf ~/.vim
+    cp -r vim_plugins/ .vim/
 }
 
 install_tmux_config()
 {
     echo "installing tmux.config"
+    cp tmux.conf ~/.tmux.conf
 }
 
 install_zshrc()
@@ -58,6 +64,7 @@ cleanup()
 {
     echo "cleaning up..."
     echo "removing unsed files..."
+    cd ~
     rm -rf vimrc-and-plugins/
 }
 
