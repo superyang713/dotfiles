22736b47dbff6b241e8b79cff8fc1f5ef0c1cbc9
superyang713 <yangdai713@aim.com>
2018-08-19 14:44:47 -0400

    set up install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,34 @@
 
 download_repo()
 {
-    echo "Downloading all config files from https://github.com/superyang713/\
-vimrc-and-plugins"
+    echo "Downloading all config files from Yang's repo"
     git clone https://github.com/superyang713/vimrc-and-plugins
+
+    echo "Preparing for vim plugins download"
     cd vimrc-and-plugins/vim_plugins/bundle/
-    echo "Downloading vim plugins"
+    echo "Downloading nerdtree..."
+    git clone --quiet https://github.com/jistr/vim-nerdtree-tabs.git
+    git clone --quiet https://github.com/scrooloose/nerdtree.git
+    echo "Downloading powerline..."
+    git clone --quiet https://github.com/powerline/powerline.git
+    echo "Downloading sensible..."
+    git clone --quiet https://github.com/tpope/vim-sensible.git
+    echo "Downloading python-mode..."
+    git clone --quiet --recursive https://github.com/python-mode/python-mode
+    echo "Downloading sparkup..."
+    git clone --quiet https://github.com/rstacruz/sparkup.git
+    echo "Downloading tlib_vim..."
+    git clone --quiet https://github.com/tomtom/tlib_vim.git
+    echo "Downloading snippets..."
+    git clone --quiet https://github.com/honza/vim-snippets.git
+    echo "Downloading snipmate..."
+    git clone --quiet https://github.com/garbas/vim-snipmate.git
+    echo "Downloading ctrlp..."
+    git clone --quiet https://github.com/kien/ctrlp.vim.git
+    echo "Downloading solarize..."
+    git clone --quiet https://github.com/altercation/vim-colors-solarized.git
+
+    cd ../../../
 }
 
 install_vimrc()
