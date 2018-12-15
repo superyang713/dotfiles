ee48cb7bcf221385acb31b45b163f14b00bbb7a5
Yang Dai <yangdai713@aim.com>
2018-12-15 13:22:46 -0500

    add emacs config files

---
+++ .emacs.d/elpa/dracula-theme-1.4.0/dracula-theme-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "dracula-theme" "1.4.0" "Dracula Theme" '((emacs "24")) :commit "1f649824d996b1ecdcc5980210b774f0cb950e9a" :authors '(("film42")) :maintainer '("film42") :url "https://github.com/dracula/emacs")
